6e99dcb4e476bb492a8b074c5198d71f54b19285
jonfrans <jonathan00234@hotmail.com>
2025-01-26 16:30:52 -0300

    Update script.sh

--- Hyprland/script.sh
+++ Hyprland/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -Syu hyprland hyprpolkitagent qt5-wayland qt6-wayland rofi-wayland waybar pipewire wireplumber --noconfirm
+sudo pacman -Syu hyprland hyprpolkitagent qt5-wayland qt6-wayland rofi-wayland waybar pipewire wireplumber ttf-space-mono-nerd otf-font-awesome --noconfirm
 
