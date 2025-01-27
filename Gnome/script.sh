cdbcf09230d451d6ef8b9745befc52a5dd317bde
jonfrans <jonathan00234@hotmail.com>
2025-01-27 20:03:20 -0300

    Create script.sh

---
+++ Gnome/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo pacman -Syu gnome gdm
+
+systemctl enable gdm
+
+gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 2
