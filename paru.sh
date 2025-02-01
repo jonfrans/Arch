bf97d9ea06c11ed7ad4eb88f410b710af1f3d86f
jonfrans <jonathan00234@hotmail.com>
2025-02-01 18:43:03 -0300

    Update paru.sh

--- paru.sh
+++ paru.sh
@@ -8,7 +8,5 @@ makepkg -si --noconfirm
 
 paru -Syu oh-my-zsh-git --noconfirm
 
-sudo cp -r /usr/share/oh-my-zsh/zshrc $HOME/
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
