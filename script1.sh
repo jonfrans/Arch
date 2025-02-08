89ea5d76b2ceea2e9f8565230bab3164abcef1d2
jonfrans <jonathan00234@hotmail.com>
2025-02-08 17:29:14 -0300

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkfs.xfs -f /dev/sda3 && mkfs.vfat /dev/sda1
+mkfs.xfs -f /dev/sda2 && mkfs.fat -F 32 /dev/sda1 && mkfs.xfs -f /dev/sda3 
 
-mkswap /dev/sda2
+mount /dev/sda2 /mnt
 
-swapon /dev/sda2
-
-mount /dev/sda3 /mnt
+mount /dev/sda3 /mnt/home
 
 mount /dev/sda1 --mkdir /mnt/efi
 
-pacstrap -K /mnt base git base-devel linux-zen linux-zen-headers linux-firmware xfsprogs sudo nano && arch-chroot /mnt
+pacstrap -K /mnt base git base-devel linux-zen linux-zen-headers linux-firmware xfsprogs sudo nano
+
+genfstab -U /mnt >> /mnt/etc/fstab
