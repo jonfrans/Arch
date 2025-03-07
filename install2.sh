3608229088d98226cffe0e42c9ae6b3fac276e93
jonfrans <jonathan00234@hotmail.com>
2025-03-07 18:33:35 -0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+cp /etc/sudoers /
+
+echo "temp ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+useradd -m temp
+echo 123 | passwd -s temp 
+
+su -l temp
+
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin/
+makepkg -si --noconfirm
+
+userdel -r temp 
+
+mv /sudoers /etc/sudoers
