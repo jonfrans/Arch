1f3fa772a87f6c9ef836595c92d9ce252ddbccac
jonfrans <jonathan00234@hotmail.com>
2025-03-07 19:50:51 -0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+###### Preparando o sistema ######
+
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+
+hwclock --systohc
+
+###### Preparando a UKI ######
 curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/pacman.conf > /etc/pacman.conf
 
 uuid="$(blkid -s UUID -o value /dev/sda2)" 
 
 echo root=UUID=$root rw rootfstype=xfs > /etc/kernel/cmdline
 
-####### Instalação do yay ######
+curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/misc/linux-zen.preset > /etc/mkninitcpio.d/linux-zen.preset
+
+mkdir -p /efi/EFI/Linux/
+
+mkinitcpio -P
+
+###### Fim ######
+
+###### Instalação do yay ######
 cp /etc/sudoers /sudoers.bak
 
 echo "temp ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
