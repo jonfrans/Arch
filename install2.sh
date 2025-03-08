ae19299f3943a584e99f56f79391c9cb29d8b12c
jonfrans <jonathan00234@hotmail.com>
2025-03-08 14:42:36 -0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -39,7 +39,7 @@ systemctl enable NetworkManager
 
 uuid="$(blkid -s UUID -o value /dev/sda2)" 
 
-echo root=UUID=$root rw rootfstype=xfs > /etc/kernel/cmdline
+echo root=UUID=$uuid rw rootfstype=xfs > /etc/kernel/cmdline
 
 curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/misc/linux-zen.preset > /etc/mkninitcpio.d/linux-zen.preset
 
