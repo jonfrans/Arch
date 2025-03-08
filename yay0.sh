b05653de00c79ae39c37260eec47454e2935e261
jonfrans <jonathan00234@hotmail.com>
2025-03-08 16:03:20 -0300

    Update yay0.sh

--- yay0.sh
+++ yay0.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-cp /etc/sudoers /sudoers.bak
+user="$(cat /user.txt)"
 
-echo "temp ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-useradd -m -s /bin/bash temp
-echo 123 | passwd -s temp 
+echo "$user ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 su -l temp -c bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/yay.sh)
 
