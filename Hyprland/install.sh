ffd8c351b1c982b45e362a36df2698894bf53023
jonfrans <jonathan00234@hotmail.com>
2025-03-07 22:46:21 -0300

    Update and rename script.sh to install.sh

---
+++ Hyprland/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pacman -Syu hyprland hyprpolkitagent qt5-wayland qt6-wayland rofi-wayland  pipewire pipewire-pulse pavucontrol wireplumber ttf-space-mono-nerd otf-font-awesome kitty --noconfirm
+
+
+
+
