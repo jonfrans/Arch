05231df6cf545ffd920a2d67b4c91ccb8467bd59
jonfrans <jonathan00234@hotmail.com>
2025-03-08 12:05:37 -0300

    Update install.sh

--- Cinnamon/install.sh
+++ Cinnamon/install.sh
@@ -4,5 +4,21 @@ su -l temp
 
 sudo pacman -Syu cinnamon cinnamon-translations
 
-yay -Syu mint-artwork --noconfirm
+echo "Você quer instalar o tema do linux mint?(Y/n)"
+read tema
 
+while true; do
+case $tema in
+  Y | y | "")
+     yay -Syu mint-artwork --noconfirm
+     break
+     ;;
+  N | n)
+     echo "Pulando essa etapa"
+     break
+     ;;
+  *)
+     echo "Insira uma opção valida(Y/n)"
+     ;; 
+esac
+done
