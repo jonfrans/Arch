79cc107284e5ed5b3814be4f00e1146212124e77
jonfrans <jonathan00234@hotmail.com>
2025-03-08 15:48:44 -0300

    Create yay0.sh

---
+++ yay0.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp /etc/sudoers /sudoers.bak
+
+echo "temp ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+useradd -m -s /bin/bash temp
+echo 123 | passwd -s temp 
+
+su -l temp -c bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/yay.sh)
+
+mv /sudoers.bak /etc/sudoers
