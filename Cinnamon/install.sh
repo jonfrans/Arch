3901caeb62b65801e684c6e15e019fae6b8118ad
jonfrans <jonathan00234@hotmail.com>
2025-03-08 19:50:19 -0300

    Update install.sh

--- Cinnamon/install.sh
+++ Cinnamon/install.sh
@@ -5,3 +5,5 @@ sudo pacman -Syu cinnamon cinnamon-translations
 yay -Syu mint-artwork --noconfirm
 
 sudo systemctl enable lightdm
+
+curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/misc/lightdm.conf > /etc/lightdm.conf
