5b07ec6046693f2b0ed635d20378e9efee68ede2
jonfrans <jonathan00234@hotmail.com>
2025-03-08 16:31:08 -0300

    Update install.sh

--- Cinnamon/install.sh
+++ Cinnamon/install.sh
@@ -5,22 +5,7 @@ sudo pacman -Syu cinnamon cinnamon-translations
 echo "Você quer instalar o tema do linux mint?(Y/n)"
 read tema
 
-while true; do
-case $tema in
-  Y | y | "")
-     yay -Syu mint-artwork --noconfirm
-     sudo systemctl enable lightdm
-     break
-     ;;
-  N | n)
-     sudo pacman -Syu lightdm lightdm-gtk-greeter
-     sudo systemctl enable lightdm
-     break
-     ;;
-  *)
-     echo "Insira uma opção valida(Y/n)"
-     ;; 
-esac
-done
+yay -Syu mint-artwork --noconfirm
+sudo systemctl enable lightdm
 
 sudo su - -c bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/uefi.sh)
