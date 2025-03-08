49316cb4417c74fb4b9f7800abeeb8a0c57edf06
jonfrans <jonathan00234@hotmail.com>
2025-03-08 17:33:27 -0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -75,4 +75,4 @@ echo "$user ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 ###### Fim ######
 
-bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main)
+bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/chaotic-aur.sh)
