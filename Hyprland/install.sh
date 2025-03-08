5fe6c948a3343699881097d1f250403bc5bec767
jonfrans <jonathan00234@hotmail.com>
2025-03-07 23:15:15 -0300

    Update install.sh

--- Hyprland/install.sh
+++ Hyprland/install.sh
@@ -2,6 +2,9 @@
 
 sudo pacman -Syu hyprland hyprpolkitagent qt5-wayland qt6-wayland rofi-wayland  pipewire pipewire-pulse pavucontrol wireplumber  kitty --noconfirm
 
-echo ""
+echo "Você vai querer uma barra de status?(Y/n)"
+read status
+
+
 
 
