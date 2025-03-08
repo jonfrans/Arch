49e73116250b2837ae28d6ed078d1e61a2e7f7a5
jonfrans <jonathan00234@hotmail.com>
2025-03-08 15:35:31 -0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -49,12 +49,14 @@ mkinitcpio -P
 
 ###### Fim ######
 
+sleep 3
+
 ###### Instalação do yay ######
 cp /etc/sudoers /sudoers.bak
 
 echo "temp ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-useradd -m temp
+useradd -m -s /bin/bash temp
 echo 123 | passwd -s temp 
 
 su -l temp -c bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/yay.sh)
