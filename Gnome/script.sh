3dd17b55d87f204d053993c7a025b1fc3b6d8e44
jonfrans <jonathan00234@hotmail.com>
2025-03-08 16:17:15 -0300

    Update script.sh

--- Gnome/script.sh
+++ Gnome/script.sh
@@ -7,3 +7,5 @@ sudo systemctl enable gdm
 gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 2
+
+sudo su - -c bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/uefi.sh)
