8da98a1931775ee70800a85668240b1e1289aea9
jonfrans <jonathan00234@hotmail.com>
2025-03-08 16:04:26 -0300

    Update yay0.sh

--- yay0.sh
+++ yay0.sh
@@ -4,6 +4,6 @@ user="$(cat /user.txt)"
 
 echo "$user ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-su -l temp -c bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/yay.sh)
+su -l $user -c bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/yay.sh)
 
 
