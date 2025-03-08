483b7ce11e7b79b19ea1b60161e6ee619cafe3d6
jonfrans <jonathan00234@hotmail.com>
2025-03-08 18:10:26 -0300

    Update DE.sh

--- DE.sh
+++ DE.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ###### Instalação do yay ######
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin/
@@ -6,7 +7,6 @@ makepkg -si --noconfirm
 ###### Fim ######
 
 echo "Você deseja usar:"
-echo "[0] Nenhuma"
 echo "[1] Hyprland"
 echo "[2] KDE Plasma"
 echo "[3] Gnome"
@@ -14,9 +14,6 @@ echo "[4] Cinnamon"
 read DE
 while true;do
 case $DE in
-    0)
-      continue
-      ;;
     1)
       bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/install.sh)
       break
