64390117f6441f358be8847fd8595a419c594d70
jonfrans <jonathan00234@hotmail.com>
2025-03-08 17:38:44 -0300

    Update install.sh

--- Cinnamon/install.sh
+++ Cinnamon/install.sh
@@ -2,7 +2,4 @@
 
 sudo pacman -Syu cinnamon cinnamon-translations
 
-yay -Syu mint-artwork --noconfirm
-sudo systemctl enable lightdm
 
-sudo su -l  -c bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/uefi.sh)
