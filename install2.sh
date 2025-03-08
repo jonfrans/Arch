50de17eeb2061300c01e0ba0a95be7c0da800f5b
jonfrans <jonathan00234@hotmail.com>
2025-03-08 15:47:58 -0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -75,18 +75,7 @@ echo $user > /user.txt
 
 ###### Fim ######
 
-###### Instalação do yay ######
-cp /etc/sudoers /sudoers.bak
 
-echo "temp ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-useradd -m -s /bin/bash temp
-echo 123 | passwd -s temp 
-
-su -l temp -c bash <(curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/yay.sh)
-
-mv /sudoers.bak /etc/sudoers
-###### Fim ######
 
 userdel -rf temp
 rm -rf /user.txt
