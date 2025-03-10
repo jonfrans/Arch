79b5f3cd022c8160ffb8020764248beb94481d51
jonfrans <jonathan00234@hotmail.com>
2025-03-09 21:16:21 -0300

    Update uefi.sh

--- uefi.sh
+++ uefi.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
 
-user="$(cat /user.txt)"
-
-mkinitcpio -P
-
-efibootmgr -c -d /dev/sda -p 1 -L "Arch Linux" -l "\EFI\Linux\arch-linux-zen.efi" -u
-
-curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/DE.sh > /home/$user/de.sh
