81a432302179ce1de45b61339ee012ab10e507ba
jonfrans <jonathan00234@hotmail.com>
2025-03-10 15:20:37 -0300

    Update install.sh

--- Hyprland/install.sh
+++ Hyprland/install.sh
@@ -8,37 +8,8 @@ mkdir -p ~/.config/hypr/
 
 curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/hyprland.conf > ~/.config/hypr/hyprland.conf
 
-echo "Você vai querer uma barra de status?(Y/n)"
-read status
-
-while true;do
- case $status in
-   Y | y | "")
-    echo "Qual você quer?"
-    echo "[1] Waybar"
-    echo "[2] Eww"
-    read bar
-
-    case $bar in
-      1)
-        pacman -Syu waybar
-        mkdir -p $home/.config/Waybar/
-        curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/Waybar/config.jsonc > ~/.config/Waybar/config.jsonc
-        curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/Waybar/style.css > ~/.config/Waybar/style.css
-        break
-        ;;
-      2)
-        echo "Chegando em breve"
-        break
-        ;;
-    break
-    ;;
-   N | n)
-    break
-    ;;
-   *)
-    echo ""
-    ;;
-done
-
+pacman -Syu waybar
+mkdir -p $home/.config/Waybar/
+curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/Waybar/config.jsonc > ~/.config/Waybar/config.jsonc
+curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/Waybar/style.css > ~/.config/Waybar/style.css
 
