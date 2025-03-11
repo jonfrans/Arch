9194f949c1a48dcce42f8f4723a85b1fbce99a20
jonfrans <jonathan00234@hotmail.com>
2025-03-11 16:27:54 -0300

    Update install.sh

--- Hyprland/install.sh
+++ Hyprland/install.sh
@@ -8,8 +8,4 @@ mkdir -p ~/.config/hypr/
 
 curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/hyprland.conf > ~/.config/hypr/hyprland.conf
 
-pacman -Syu waybar
-mkdir -p $home/.config/Waybar/
-curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/Waybar/config.jsonc > ~/.config/Waybar/config.jsonc
-curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/Waybar/style.css > ~/.config/Waybar/style.css
 
