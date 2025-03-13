b1b402484bd40f5fb6eff632ac35c6ac3a7412e1
jonfrans <jonathan00234@hotmail.com>
2025-03-13 16:41:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ read choice
 
 case $choice in
   Y | y)
-   parted /dev/sda --script mklabel gpt mkpart P1 fat32 1MiB 1GiB mkpart P2 xfs 1GiB 30GiB mkpart P3 xfs 30GiB 100%
+   parted /dev/sda --script mklabel gpt mkpart EFI fat32 1MiB 1GiB mkpart root xfs 1GiB 30GiB mkpart home xfs 30GiB 100%
    parted /dev/sda --script set 1 esp on set 3 linux-home on
    parted /dev/sda --script type 1 C12A7328-F81F-11D2-BA4B-00A0C93EC93B type 2 4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709 type 3 933AC7E1-2EB4-4F13-B844-0E14E2AEF915
    break
