393cbb35d7db04963f458585a825d0b07dd16920
jonfrans <jonathan00234@hotmail.com>
2025-03-13 17:32:45 -0300

    Update install.sh

--- i3/install.sh
+++ i3/install.sh
@@ -2,3 +2,12 @@
 
 sudo pacman -Syu i3-wm lightdm lightdm-gtk-greeter
 
+git clone https://github.com/elkowar/eww
+
+cd eww
+
+cargo build --release --no-default-features --features x11
+
+cd ./target/release
+
+chmod +x ./eww
