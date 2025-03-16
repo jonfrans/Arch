375481d9044060fd34a9827dbbac4ac6a52f6a3b
jonfrans <jonathan00234@hotmail.com>
2025-03-16 13:33:16 -0300

    Update install.sh

--- i3/install.sh
+++ i3/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syu i3-wm lightdm lightdm-gtk-greeter rustup gtk3 gtk-layer-shell pango gdk-pixbuf2 libdbusmenu-gtk3 cairo glib2 gcc-libs glibc --noconfirm
+sudo pacman -Syu i3-wm lightdm lightdm-gtk-greeter feh rustup gtk3 gtk-layer-shell pango gdk-pixbuf2 libdbusmenu-gtk3 cairo glib2 gcc-libs glibc --noconfirm
 
 git clone https://github.com/elkowar/eww
 
