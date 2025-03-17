881cd9f49eee9ef42e6670bf5629072349088d64
jonfrans <jonathan00234@hotmail.com>
2025-03-17 12:34:14 -0300

    Update install.sh

--- Hyprland/install.sh
+++ Hyprland/install.sh
@@ -7,13 +7,3 @@ sudo systemctl enable sddm
 mkdir -p ~/.config/hypr/
 
 curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/hyprland.conf > ~/.config/hypr/hyprland.conf
-
-git clone https://github.com/elkowar/eww
-
-cd eww
-
-cargo build --release --no-default-features --features=wayland
-
-cd ./target/release
-
-chmod +x ./eww
