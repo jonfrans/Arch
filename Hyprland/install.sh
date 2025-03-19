b5bc7a1110e24f722b80a10d85dfcdc63aa4f5a2
jonfrans <jonathan00234@hotmail.com>
2025-03-19 12:36:28 -0300

    Update install.sh

--- Hyprland/install.sh
+++ Hyprland/install.sh
@@ -2,8 +2,16 @@
 
 sudo pacman -Syu hyprland hyprpolkitagent qt5-wayland swww sddm qt6-wayland rofi-wayland xdg-desktop-portal-hyprland pipewire pipewire-pulse pavucontrol wireplumber kitty --noconfirm
 
+sudo pacman -S gtk3 gtk-layer-shell pango gdk-pixbuf2 libdbusmenu-gtk3 cairo glib2 gcc-libs glibc rustup --noconfirm
+
 sudo systemctl enable sddm
 
 mkdir -p ~/.config/hypr/
 
 curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/hyprland.conf > ~/.config/hypr/hyprland.conf
+
+git clone https://github.com/elkowar/eww
+
+cd eww
+
+cargo build --release --no-default-features --features=wayland
