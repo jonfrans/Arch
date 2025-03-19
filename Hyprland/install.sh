7690fea64367474e30968508e2b54c7d4cd23c9a
jonfrans <jonathan00234@hotmail.com>
2025-03-19 13:01:28 -0300

    Update install.sh

--- Hyprland/install.sh
+++ Hyprland/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syu hyprland hyprpolkitagent qt5-wayland swww sddm qt6-wayland rofi-wayland xdg-desktop-portal-hyprland pipewire pipewire-pulse pavucontrol wireplumber kitty --noconfirm
+sudo pacman -Syu hyprland hyprpolkitagent qt5-wayland swww sddm qt6-wayland xdg-desktop-portal-hyprland pipewire pipewire-pulse wireplumber kitty --noconfirm
 
 sudo pacman -S gtk3 gtk-layer-shell pango gdk-pixbuf2 libdbusmenu-gtk3 cairo glib2 gcc-libs glibc rustup --noconfirm
 
