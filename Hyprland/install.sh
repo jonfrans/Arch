9b407897c5596e3af22fa80bdf57b3130e6107ae
jonfrans <jonathan00234@hotmail.com>
2025-03-23 18:15:32 -0300

    Update install.sh

--- Hyprland/install.sh
+++ Hyprland/install.sh
@@ -2,10 +2,6 @@
 
 sudo pacman -Syu hyprland hyprpolkitagent qt5-wayland swww sddm qt6-wayland xdg-desktop-portal-hyprland pipewire pipewire-pulse wireplumber kitty --noconfirm
 
-sudo pacman -S gtk3 gtk-layer-shell pango gdk-pixbuf2 libdbusmenu-gtk3 cairo glib2 gcc-libs glibc rustup --noconfirm
-
-sudo systemctl enable sddm
-
 mkdir -p ~/.config/hypr/
 
 curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/hyprland.conf > ~/.config/hypr/hyprland.conf
