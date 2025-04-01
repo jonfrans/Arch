afa3f1156275656c60d21fc041f9eeee1d6fc77d
jonfrans <jonathan00234@hotmail.com>
2025-04-01 14:38:24 -0300

    Update install.sh

--- Hyprland/install.sh
+++ Hyprland/install.sh
@@ -2,9 +2,11 @@
 
 sudo pacman -Syu hyprland hyprpolkitagent qt5-wayland swww sddm qt6-wayland xdg-desktop-portal-hyprland pipewire pipewire-pulse wireplumber kitty --noconfirm
 
-mkdir -p ~/.config/hypr/
+mkdir -p ~/.config/hypr ~/.config/waybar
 
 curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/hyprland.conf > ~/.config/hypr/hyprland.conf
 
+curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/Waybar/config.jsonc > ~/.config/waybar/config.jsonc
 
+curl -s https://raw.githubusercontent.com/jonfrans/Arch/refs/heads/main/Hyprland/Waybar/style.css > ~/.config/waybar/style.css
 
